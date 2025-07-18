12eeb16c1a9df400fdf89e3191cdc3c8c531ca05
AleixMT <aleixaretra@gmail.com>
2025-07-18 12:37:54 +0200

    ADDED: possibilities to run workflows with silenced code

--- demo/run_workflow.sh
+++ demo/run_workflow.sh
@@ -2,7 +2,9 @@ export PROJECT_FOLDER="$(cd "$(dirname "$(realpath "$0")")/.." &>/dev/null && pw
 
 # Set your variables
 #JENKINS_URL="http://localhost:$(cat "${PROJECT_FOLDER}/secrets/FIREWALL_PORT_EXTERNAL")"
-JENKINS_URL="https://$(cat "${PROJECT_FOLDER}/secrets/HOSTNAME")"
+#JENKINS_URL="https://$(cat "${PROJECT_FOLDER}/secrets/HOSTNAME")"
+JENKINS_URL="http://$(cat "${PROJECT_FOLDER}/secrets/HOSTNAME")"
+
 USER="Justicier"
 API_TOKEN="$(cat $PROJECT_FOLDER/secrets/JENKINS_API_TOKEN)"
 JOB="run-justicier"
