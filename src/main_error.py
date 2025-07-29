f9028f87796c0304d6bb9f41b57b9510d51c9a01
AleixMT <aleixaretra@gmail.com>
2025-07-29 16:10:57 +0200

    CHANGED: moved code into condition, so it does not fail if admin folder is not created

--- src/main_error.py
+++ src/main_error.py
@@ -28,19 +28,21 @@ def main():
     args = parser.parse_args()
     update_list_item_field(args.request, {"Estatworkflow": "Error"})
 
-    if os.path.isdir(ADMIN_LOG_FOLDER):
+    if os.path.isdir(ADMIN_LOG_FOLDER):  # Only upload when the folder is detected
         SUPERVISOR_LOG_PATH = get_first_log_path(ADMIN_LOG_FOLDER)
+        token_manager = get_token_manager()
+        sharepoint_domain = read_secret('SHAREPOINT_DOMAIN')
+        site_name = read_secret('SITE_NAME')
+        site_id = get_site_id(token_manager, sharepoint_domain, site_name)
+        drive_id = get_drive_id(token_manager, site_id, drive_name="Documents")
 
-    token_manager = get_token_manager()
-    sharepoint_domain = read_secret('SHAREPOINT_DOMAIN')
-    site_name = read_secret('SITE_NAME')
-    site_id = get_site_id(token_manager, sharepoint_domain, site_name)
-    drive_id = get_drive_id(token_manager, site_id, drive_name="Documents")
+        upload_file(token_manager,
+                    drive_id,
+                    os.path.join(read_secret("SHAREPOINT_FOLDER_OUTPUT"), "_admin_logs",
+                                 os.path.basename(SUPERVISOR_LOG_PATH)),
+                    SUPERVISOR_LOG_PATH)
 
-    upload_file(token_manager,
-                drive_id,
-                os.path.join(read_secret("SHAREPOINT_FOLDER_OUTPUT"), "_admin_logs", os.path.basename(SUPERVISOR_LOG_PATH)),
-                SUPERVISOR_LOG_PATH)
+
 
 
 if __name__ == "__main__":
